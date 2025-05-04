5e12e1e78a131bccb30eecca37cbd75d9be3974b
Altin Maloku <altin7maloku@gmail.com>
2025-05-04 18:58:30 +0200

    Update EditRole to include display_name in data

--- src/Resources/RoleResource/Pages/EditRole.php
+++ src/Resources/RoleResource/Pages/EditRole.php
@@ -18,7 +18,7 @@ class EditRole extends EditRecord
     protected function getActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 
@@ -33,10 +33,10 @@ class EditRole extends EditRecord
             ->unique();
 
         if (Arr::has($data, Utils::getTenantModelForeignKey())) {
-            return Arr::only($data, ['name', 'guard_name', Utils::getTenantModelForeignKey()]);
+            return Arr::only($data, ['name', 'display_name', 'guard_name', Utils::getTenantModelForeignKey()]);
         }
 
-        return Arr::only($data, ['name', 'guard_name']);
+        return Arr::only($data, ['name', 'display_name', 'guard_name']);
     }
 
     protected function afterSave(): void
