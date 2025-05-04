c47c14509c7ea2638a51613ff655625e7e8c4576
Altin Maloku <altin7maloku@gmail.com>
2025-05-04 18:54:42 +0200

    Remove create route from RoleResource

--- src/Resources/RoleResource.php
+++ src/Resources/RoleResource.php
@@ -137,7 +137,7 @@ class RoleResource extends Resource implements HasShieldPermissions
     {
         return [
             'index' => Pages\ListRoles::route('/'),
-            'create' => Pages\CreateRole::route('/create'),
+            // 'create' => Pages\CreateRole::route('/create'),
             'view' => Pages\ViewRole::route('/{record}'),
             'edit' => Pages\EditRole::route('/{record}/edit'),
         ];
