ea51146257f2489e9cb201f6dac0e08d7ec317f2
altin7m <24477391+altin7m@users.noreply.github.com>
2026-02-16 04:51:32 +0000

    Fix styling

--- src/Resources/Roles/RoleResource.php
+++ src/Resources/Roles/RoleResource.php
@@ -26,7 +26,6 @@ use Filament\Schemas\Schema;
 use Filament\Support\Enums\FontWeight;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\Unique;
 
 class RoleResource extends Resource
